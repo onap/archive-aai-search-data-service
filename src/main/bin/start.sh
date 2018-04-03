9cd5240b1f89eee38b0af9cfa25d74c1ee8b8b52
Shwetank Dave <shwetank.dave@amdocs.com>
2018-04-03 16:30:01 -0400

    [AAI-804] Updating files to fix search service.
    Issue-ID: AAI-804
    Change-Id: Ie50f821cd235ccd5c627c11294da4b29bf29fc16
    Signed-off-by: Shwetank Dave <shwetank.dave@amdocs.com>

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASEDIR="/opt/app/search-data-service/"
+BASEDIR="/opt/app/search-data-service"
 
 if [ -z "$CONFIG_HOME" ]; then
 	echo "CONFIG_HOME must be set in order to start up process"
@@ -11,22 +11,22 @@ if [ -z "$KEY_STORE_PASSWORD" ]; then
 	echo "KEY_STORE_PASSWORD must be set in order to start up process"
 	exit 1
 else
-	echo "server.ssl.key-store-password=$KEY_STORE_PASSWORD" >> $BASEDIR/config/application.properties
+	## Extract java jar and DEOBFUSCATE the password.
+	sudo jar xf $BASEDIR/search-data-service-package.jar
+	sudo java -cp $BASEDIR/BOOT-INF/lib/jetty-util-9.4.8.v20171121.jar org.eclipse.jetty.util.security.Password $KEY_STORE_PASSWORD > $BASEDIR/pass.txt 2>> $BASEDIR/pass.txt
+	PASS=`sed "2q;d" $BASEDIR/pass.txt`
+	sudo rm $BASEDIR/pass.txt
 fi
 
 ## tomcat_keystore to p12
-## keytool -importkeystore -deststorepass onapSecret -destkeypass onapSecret -srckeystore /opt/app/search-data-service/config/auth/tomcat_keystore -destkeystore /opt/app/search-data-service/config/auth/onap.p12 -deststoretype PKCS12 -srcstorepass onapSecret
-keytool -importkeystore -noprompt -deststorepass onapSecret -destkeypass onapSecret -srckeystore /opt/app/search-data-service/config/auth/tomcat_keystore -destkeystore /opt/app/search-data-service/config/auth/onap.p12 -deststoretype PKCS12 -srcstorepass onapSecret
+keytool -importkeystore -noprompt -deststorepass $PASS -destkeypass $PASS -srckeystore $BASEDIR/config/auth/tomcat_keystore -destkeystore $BASEDIR/config/auth/onap.p12 -deststoretype PKCS12 -srcstorepass $PASS
 
 ## import into cacerts
-## keytool -importkeystore -deststorepass changeit -destkeypass changeit -destkeystore $JAVA_HOME/jre/lib/security/cacerts -srckeystore /opt/app/search-data-service/config/auth/onap.p12 -srcstoretype PKCS12 -srcstorepass onapSecret  -alias tomcat
-keytool -importkeystore -noprompt -deststorepass changeit -destkeypass changeit -destkeystore $JAVA_HOME/jre/lib/security/cacerts -srckeystore /opt/app/search-data-service/config/auth/onap.p12 -srcstoretype PKCS12 -srcstorepass onapSecret  -alias tomcat
-
-
+sudo keytool -importkeystore -noprompt -deststorepass changeit -destkeypass changeit -destkeystore /$JAVA_HOME/jre/lib/security/cacerts -srckeystore $BASEDIR/config/auth/onap.p12 -srcstoretype PKCS12 -srcstorepass $PASS -alias tomcat
 
 PROPS="$PROPS -Dlogback.configurationFile=$BASEDIR/bundleconfig/etc/logback.xml"
-#PROPS="$PROPS -Dserver.port=9509"
 PROPS="$PROPS -DCONFIG_HOME=$CONFIG_HOME"
+PROPS="$PROPS -DKEY_STORE_PASSWORD=$KEY_STORE_PASSWORD"
 JVM_MAX_HEAP=${MAX_HEAP:-1024}
 
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 $PROPS -jar $BASEDIR/search-data-service-1.2.0-SNAPSHOT.jar --spring.config.location=$BASEDIR/config/application.properties
+java $PROPS -jar $BASEDIR/search-data-service-package.jar
--- src/main/java/org/onap/aai/sa/Application.java
+++ src/main/java/org/onap/aai/sa/Application.java
@@ -1,53 +1,25 @@
-/**
- * ============LICENSE_START=======================================================
- * org.onap.aai
- * ================================================================================
- * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
- * Copyright © 2017-2018 Amdocs
- * ================================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ============LICENSE_END=========================================================
- */
 package org.onap.aai.sa;
 
-// import org.eclipse.jetty.util.security.Password;
-
+import org.eclipse.jetty.util.security.Password;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import java.util.HashMap;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-//	public static String[] deobfuscateArgs(String[] args, String ... attrnames) {
-//
-//		String[] deobfuscatedArgs = args.clone();
-//
-//		Password.deobfuscate("HI");
-//
-//		//System.setProperty(arg0, arg1)
-//
-//		return deobfuscatedArgs;
-//	}
-//
+
     public static void main(String[] args) {
-    	
-    	//server.ssl.key-store-password=onapSecret
-    	//server.ssl.key-password=onapSecret
-//    	args = new String[]{"-Dserver.ssl.key-store-password", "onapSecret",
-//    			"-Dserver.ssl.key-password", "onapSecret"};
-    	
-    	SpringApplication.run(Application.class, args);
 
-    	//deobfuscateArgs(args, "server.ssl.key-store-password", "server.ssl.key-password"));
+        String keyStorePassword = System.getProperty("KEY_STORE_PASSWORD");
+        if(keyStorePassword==null || keyStorePassword.isEmpty()){
+            throw new RuntimeException("Env property KEY_STORE_PASSWORD not set");
+        }
+        HashMap<String, Object> props = new HashMap<>();
+        props.put("server.ssl.key-store-password", Password.deobfuscate(keyStorePassword));
+        new Application().configure(new SpringApplicationBuilder (Application.class).properties(props)).run(args);
     }
 }
