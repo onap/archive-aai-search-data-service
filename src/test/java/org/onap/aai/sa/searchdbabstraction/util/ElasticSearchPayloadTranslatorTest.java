7d5655c8b82368867ea318172cb92a474d4ad247
Edwin Lawrance <Edwin.Lawrance@amdocs.com>
2018-07-12 16:28:57 +0100

    Updating Search service to be ES 6.1.2 compliant
    Payload to Elastic Search is translated to comply ES6.1.2
    PUT and POST calls now have content-type header
    
    Change-Id: I2a44a8a9999ec01a3bad1fb6999fe35bb6ef70d1
    Issue-ID: AAI-1376
    Signed-off-by: Edwin Lawrance <Edwin.Lawrance@amdocs.com>

---
+++ src/test/java/org/onap/aai/sa/searchdbabstraction/util/ElasticSearchPayloadTranslatorTest.java
@@ -0,0 +1,51 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright © 2017-2018 Amdocs
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.sa.searchdbabstraction.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.aai.sa.rest.TestUtils;
+
+public class ElasticSearchPayloadTranslatorTest {
+	
+	private final String SIMPLE_DOC_SCHEMA_JSON = "src/test/resources/json/simpleDocument.json";
+
+	@Before
+	public void setup() throws Exception {
+		System.setProperty("CONFIG_HOME", System.getProperty("user.dir")+ File.separator + "src/test/resources/json/");
+	}
+	
+	@Test
+	public void testPayloadTranslation_FromStringToText() throws Exception {
+		File schemaFile = new File(SIMPLE_DOC_SCHEMA_JSON);
+	    String documentJson = TestUtils.readFileToString(schemaFile);
+	    assertTrue(documentJson.contains("\"data-type\":\"string\""));
+		assertTrue(documentJson.contains("\"searchable\":true"));
+		String translatedPayload = ElasticSearchPayloadTranslator.translateESPayload(documentJson);
+		assertTrue(translatedPayload.contains("\"data-type\":\"text\""));
+		assertTrue(translatedPayload.contains("\"index\":true"));
+	}
+}
